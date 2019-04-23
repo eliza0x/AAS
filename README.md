2cfc34ed2670072f1a47a322c6fd0722ab7227bc
eliza0x <me@eliza.link>
2019-04-23 18:32:15 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Anti applicant system
+====
---
+++ index.js
@@ -0,0 +1,40 @@
+const puppeteer = require('puppeteer');
+const url = 'https://l508-08.is.oit.ac.jp/tsh/';
+ 
+(async function(){
+    const browser = await puppeteer.launch({
+        headless: true
+    });
+    const page = await browser.newPage();
+    const response = await page.goto(url);
+
+    const votes = await page.evaluate(() => {
+      // 研究室情報が載ってるテーブル
+      const table = Array.from(document.querySelectorAll('table table'))[1];
+
+      // 各研究室のリスト
+      const labos = Array.from(table.querySelectorAll('tbody tr td tbody tr'));
+
+      return labos.map(line => {
+        const line_arr   = Array.from(line.querySelectorAll('td'));
+        /*
+        const labo_name  = line_arr[0].innerHTML;
+        const labo_limit = line_arr[2].innerHTML;
+        const labo_cnt   = line_arr[3].innerHTML;
+        return {name: labo_name, cnt: labo_cnt, limit: labo_limit};
+        */
+        return line_arr.map(line=>line.innerHTML)
+      });
+    });
+    // votes.map(x => console.log(x.name+': '+x.cnt+'/'+x.limit))
+    votes.map(x => console.log(x+'\n'))
+
+    await browser.close();
+})();
+
+// <tr class="odd">
+//  <td>hoge</td>
+//  <td>情報通信ネットワーク</td>
+//  <td style="text-align:center;">10</td>
+//  <td style="text-align:center;"><font color="red"><b>11</b></font>(1,0,0,10)</td>
+// </tr>
